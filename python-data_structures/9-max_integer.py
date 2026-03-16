0906b00166b695b3b43534f9a4079dfba945017e
Ange-Liana <l.batsinde@alustudent.com>
2026-03-16 10:14:21 -0700

    maximum

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    first_char = sentence[0] if sentence else None
-    return (len(sentence), first_char)
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max_num = my_list[0]
+
+    for num in my_list:
+        if num > max_num:
+            max_num = num
+
+    return max_num
