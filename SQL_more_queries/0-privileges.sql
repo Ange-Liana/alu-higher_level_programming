ae0cdd2722b420b0098f342576fd0f9ce1f24df8
Ange-Liana <l.batsinde@alustudent.com>
2026-04-03 03:49:37 -0700

    file 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- Show grants for user_0d_1
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
--- Show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
